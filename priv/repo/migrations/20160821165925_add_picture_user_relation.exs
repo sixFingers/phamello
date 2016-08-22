eddf7e253abbdb202688a6ab5ae6fd9fd2bdc6f1
sixFingers <ignazio.setti@gmail.com>
2016-08-22 16:44:07 +0200

    Add index on picture-user relation

--- priv/repo/migrations/20160821165925_add_picture_user_relation.exs
+++ priv/repo/migrations/20160821165925_add_picture_user_relation.exs
@@ -5,5 +5,7 @@ defmodule Phamello.Repo.Migrations.AddPictureUserRelation do
     alter table(:pictures) do
       add :user_id, references(:users)
     end
+
+    create index(:pictures, [:user_id])
   end
 end
