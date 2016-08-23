d0fbca59d774690c5d98c1ffbc1f21f2a4300968
sixFingers <ignazio.setti@gmail.com>
2016-08-24 00:32:15 +0200

    Fix typo

--- config/prod.exs
+++ config/prod.exs
@@ -4,7 +4,7 @@ config :phamello, Phamello.Endpoint,
   url: [scheme: "https", host: "gentle-wildwood-53699.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]]
 
-config :hello_phoenix, HelloPhoenix.Repo,
+config :phamello, HelloPhoenix.Repo,
   adapter: Ecto.Adapters.Postgres,
   url: System.get_env("DATABASE_URL"),
   pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
