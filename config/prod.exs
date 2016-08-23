9eea488b9f7fbe88569c702af7e2cb4c7a1411e9
sixFingers <ignazio.setti@gmail.com>
2016-08-24 00:13:56 +0200

    Basic setup

--- config/prod.exs
+++ config/prod.exs
@@ -1,4 +1,14 @@
 use Mix.Config
 
+config :phamello, Phamello.Endpoint,
+  url: [scheme: "https", host: "mysterious-meadow-6277.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]]
+
+config :hello_phoenix, HelloPhoenix.Repo,
+  adapter: Ecto.Adapters.Postgres,
+  url: System.get_env("DATABASE_URL"),
+  pool_size: String.to_integer(System.get_env("POOL_SIZE") || "10"),
+  ssl: true
+
 config :logger, level: :info
 
