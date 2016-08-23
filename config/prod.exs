798627352da7f5af33a46121ec1cec546f2ee0e9
sixFingers <ignazio.setti@gmail.com>
2016-08-24 00:10:14 +0200

    Cleanup

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,4 @@
 use Mix.Config
 
-config :phamello, Phamello.Endpoint,
-  http: [port: {:system, "PORT"}],
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/manifest.json"
-
 config :logger, level: :info
 
